c393f6cc391c6f520986f8310987c715aded44a1
vikashfernando <vikashdeshitha@gmail.com>
2026-09-08 07:27:11 +0100

    updated functions

--- 1-py for beg/mindmath/1.py
+++ 1-py for beg/mindmath/1.py
@@ -1,5 +1,5 @@
 #data
-validInputs=["1","2","3","4","5","6","#","$"]
+validInputs=["1","2","3","4","5","6","#"]
 
 
 #functions
@@ -22,28 +22,60 @@ def mainMenu():
     print(" ")
     print("*"*40)
 
-
-
-
 # function2 - get inputs from user
 def getInput():
     userInput=input("enter: ")
     inputValidation(userInput)
 
-
-#function3 - validate inputs
-def inputValidation(num):
-    if num in validInputs:
+# function3 - validate inputs
+def inputValidation(userInput):
+    if userInput in validInputs:
         print("valid input")
-        return True
+        operations(userInput)
     else:
         print("invalid input")
 
+# function4 - operations
+def operations(userInput):
+    if userInput=="1":
+        print("add mode")
+        number1=input("number 1: ")
+        number2=input("number 2: ")
+        add(number1,number2)
+    elif userInput=="2":
+        print("sub mode")
+        number1=input("number 1: ")
+        number2=input("number 2: ")
+        sub(number1,number2)
+    elif userInput=="3":
+        print("mul mode")
+        number1=input("number 1: ")
+        number2=input("number 2: ")
+        mul(number1,number2)
+    elif userInput=="4":
+        print("div mode")
+        number1=input("number 1: ")
+        number2=input("number 2: ")
+        div(number1,number2)
+    elif userInput=="5":
+        print("pow mode")
+        number1=input("number 1: ")
+        number2=input("number 2: ")
+        pow(number1,number2)
+    elif userInput=="6":
+        print("rem mode")
+        number1=input("number 1: ")
+        number2=input("number 2: ")
+        rem(number1,number2)
+    elif userInput=="$":
+        print("$ detected")
+    else:
+        print("# detected")
 
-
-
-
-
+# function5 - to get 2 inputs
+def getOperationsInputs():
+    number1=input("number 1: ")
+    number2=input("number 2: ")
 
 
 
@@ -55,30 +87,30 @@ def inputValidation(num):
         
 #function4 - "+"
 def add(number1,number2):
-    answer=number1+number2
+    answer=int(number1)+int(number2)
     return answer
 
 #function5 - "-"
 def sub(number1,number2):
-    answer=number1-number2
+    answer=int(number1)-int(number2)
     return answer
 
 #function6 - "*"
 def mul(number1,number2):
-    answer=number1*number2
+    answer=int(number1)*int(number2)
     return answer
 
 #function7 - "/"
 def div(number1,number2):
-    answer=number1/number2
+    answer=int(number1)/int(number2)
     return answer
 
-def div(number1,number2):
-    answer=number1**number2
+def pow(number1,number2):
+    answer=int(number1)**int(number2)
     return answer
 
-def div(number1,number2):
-    answer=number1%number2
+def rem(number1,number2):
+    answer=int(number1)%int(number2)
     return answer
 
 
